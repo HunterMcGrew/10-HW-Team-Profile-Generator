c60510dfbc251400a7459075aef7ada86bd0f743
HunterLM <hunter109@gmail.com>
2022-04-27 21:36:01 -0500

    started page-template

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const express = require('express');
+const generateHtml = require('./src/page-template');
 
 // import modules from classes
 const Employee = require('./lib/Employee');
---
+++ src/page-template.js
@@ -0,0 +1,28 @@
+// function to generate index.html 
+
+function generateHtml(data) {
+
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+    
+        <header id="header">
+            <h1>My Team</h1>
+        </header>
+    
+        <main id="main">
+    
+        </main>
+        
+        <script src="./index.js"></script>
+    </body>
+    </html>
+    `
+}
